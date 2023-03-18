f033acfac9aa23c9ba425ec8aa60d826574d916c
Alexander Luna <alexfox_2000@hotmail.com>
2023-03-18 15:07:06 -0500

    in the beninging

---
+++ main.js
@@ -0,0 +1,20 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <h1>Hello World</h1>
+  </div>
+`;
+
+// setupElement(element)
+/*
+export function setupCounter(element) {
+  let counter = 0
+  const setCounter = (count) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener('click', () => setCounter(counter + 1))
+  setCounter(0)
+}
+*/
