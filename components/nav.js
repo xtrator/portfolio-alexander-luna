666ee291c06e2be9eef4b5be85bd3cecd82da808
Alexander Luna <alexfox_2000@hotmail.com>
2023-03-19 15:22:19 -0500

    :))

--- components/nav.js
+++ components/nav.js
@@ -2,17 +2,17 @@ import { setupModal } from "./modal";
 
 export function setupNav(element) {
   element.innerHTML = /* HTML */ `<div
-    class="justify-between py-4 text-center lg:flex"
-  >
-    <h1 class="font-[Papyrus] text-4xl text-pink-600 ">Alexander Luna</h1>
-    <div class="hidden gap-4 text-2xl lg:flex xl:gap-12">
-      <a class="hover:text-pink-600" href="#info">About Me</a>
-      <a class="hover:text-pink-600" href="#projects-section">Projects</a>
-      <a class="hover:text-pink-600" href="#experience-section">Experience</a>
-      <a class="hover:text-pink-600" href="#footer">Contact</a>
+      class="justify-between py-4 text-center lg:flex"
+    >
+      <h1 class="font-[Papyrus] text-4xl text-pink-600 ">Alexander Luna</h1>
+      <div class="hidden gap-4 text-2xl lg:flex xl:gap-12">
+        <a class="hover:text-pink-600" href="#info">About Me</a>
+        <a class="hover:text-pink-600" href="#projects-section">Projects</a>
+        <a class="hover:text-pink-600" href="#experience-section">Experience</a>
+        <a class="hover:text-pink-600" href="#footer">Contact</a>
+      </div>
     </div>
-    <div id="modal"></div>
-  </div>`;
+    <div id="modal"></div>`;
 
   setupModal(document.querySelector("#modal"));
 }
