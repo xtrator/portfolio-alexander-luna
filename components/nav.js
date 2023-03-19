7a33f85aa0156805a15f27dcbdaf732dd5332157
Alexander Luna <alexfox_2000@hotmail.com>
2023-03-19 14:48:38 -0500

    :)

--- components/nav.js
+++ components/nav.js
@@ -1,13 +1,13 @@
 export function setupNav(element) {
   element.innerHTML = /* HTML */ `<div
-    class="justify-between items-end py-4 lg:flex text-center"
+    class="justify-between py-4 text-center lg:flex"
   >
-    <h1 class="text-4xl text-pink-600 ">Alexander Luna</h1>
-    <div class="text-2xl gap-4 xl:gap-12 hidden lg:flex">
-      <p>About Me</p>
-      <p>Projects</p>
-      <p>Experience</p>
-      <p>Contact</p>
+    <h1 class="font-[Papyrus] text-4xl text-pink-600 ">Alexander Luna</h1>
+    <div class="hidden gap-4 text-2xl lg:flex xl:gap-12">
+      <a class="hover:text-pink-600" href="#info">About Me</a>
+      <a class="hover:text-pink-600" href="#projects-section">Projects</a>
+      <a class="hover:text-pink-600" href="#experience-section">Experience</a>
+      <a class="hover:text-pink-600" href="#footer">Contact</a>
     </div>
   </div>`;
 }
